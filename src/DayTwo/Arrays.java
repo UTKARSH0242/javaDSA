3744b736f9799a02f21349c321144d2fdbe29cd2
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-06-02 21:59:04 +0530

    commit

--- src/DayTwo/Arrays.java
+++ src/DayTwo/Arrays.java
@@ -1,2 +1,17 @@
-package DayTwo;public class Arrays {
+package DayTwo;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ageList = new int[n];
+        for (int i = 0; i<n; i++){
+            ageList[i] = sc.nextInt();
+        }
+        for (int i = 0; i<n; i++){
+            System.out.println("Age of roll no "+ (i +1) +" is "+ ageList[i]);
+        }
+    }
 }
