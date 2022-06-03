ef53b7b05944ad9a9d22e0c9779dec58c400eb30
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-06-03 14:17:42 +0530

    commit

---
+++ src/DayTwo/greaterAverage.java
@@ -0,0 +1,28 @@
+package DayTwo;
+
+import java.util.Scanner;
+
+public class greaterAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The number of element");
+        int n = sc.nextInt();
+        int[] marks = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the " + i + " th number");
+            marks[i] = sc.nextInt();
+
+//            double averageMarks = marks[i]/n;
+//            System.out.println(averageMarks);
+            sum += marks[i];
+        }
+        System.out.println(sum);
+        double averageMarks = sum / n;
+        for (int j = 0; j < n; j++) {
+            if (marks[j] > averageMarks) {
+                System.out.println(marks[j]);
+            }
+        }
+    }
+}
