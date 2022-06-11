9ea71b64c492e7f0ff2266b4fa31690e220e9c32
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-06-11 14:24:49 +0530

    commit

---
+++ src/Arrays/sortedArrayOrNot.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class sortedArrayOrNot {
+    public static boolean sortedOrNot( int a[]) {
+
+        if (a.length == 0 || a.length == 1)
+            return true;
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < a[i - 1]) {
+                return false;
+
+            }
+        }
+        return false;
+    }
+
+            public static void main (String[] args){
+                int a[] = {3, 8, 4, 5, 0, 1, 7};
+                if (sortedOrNot(a) != false) {
+                    System.out.println("Yes Array is Sorted");
+                } else System.out.println("Array is not Sorted");
+
+            }
+
+        }
