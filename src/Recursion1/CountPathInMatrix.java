7b02c5b90c655fc8f5838531466c4c984ee21962
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-06-19 19:12:40 +0530

    commit

---
+++ src/Recursion1/CountPathInMatrix.java
@@ -0,0 +1,18 @@
+package Recursion1;
+
+public class CountPathInMatrix {
+    static int countInPath(int n , int m){
+        if (n == 1 || m == 1) return 1;
+
+        int rightSide = countInPath(n , m-1);
+        int downSide = countInPath(n-1, m);
+
+        return rightSide + downSide;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 6;
+        System.out.println(countInPath(n, m));
+
+    }
+}
