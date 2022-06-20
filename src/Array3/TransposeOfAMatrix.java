b128f289c64b884429be199fcbda9efa515adc03
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-06-20 12:24:59 +0530

    commit

--- src/Array3/TransposeOfAMatrix.java
+++ src/Array3/TransposeOfAMatrix.java
@@ -1,10 +1,43 @@
 package Array3;
 
+import java.util.Scanner;
+
 public class TransposeOfAMatrix {
-    static void transposeOfAMatrix(int a[][]){
+    static void printMatrix(int a[][]) {
         int n = a.length;
-        for (int i = 0; i < n; i++) {
-            
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                System.out.print(a[i][j] +" ");
+            }
+            System.out.println();
         }
+    }
+
+    static void transposeOfAMatrix(int a[][]) {
+        int n = a.length;
+
+        for(int i = 0; i<n; i++) {
+            for(int j = i; j<n; j++) {
+                int swap = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = swap;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        int a[][] = {
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		};
+
+printMatrix(a);
+
+		transposeOfAMatrix(a);
+//        System.out.println(transposeOfAMatrix(a);
+        printMatrix(a);
     }
 }
