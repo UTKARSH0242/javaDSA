e75b44edb907de32fca91fff7c1ca98d46829575
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-06-20 13:19:39 +0530

    commit

--- src/Array3/SearchInTwoDMatrix.java
+++ src/Array3/SearchInTwoDMatrix.java
@@ -1,7 +1,7 @@
 package Array3;
 
 public class SearchInTwoDMatrix {
-    static boolean searchInTwoDMatrix(int matrix[][] , int target) {
+    static boolean searchInTwoDMatrix(int[][] matrix, int target) {
 
         int row = 0;
         int cols = matrix[0].length - 1;
@@ -18,7 +18,7 @@ public class SearchInTwoDMatrix {
         return false;
     }
     public static void main(String[] args) {
-        int matrix[][] = {
+        int[][] matrix = {
                 {1, 4, 5, 7},
                 {2, 5, 6, 9},
                 {6, 10, 11, 13},
