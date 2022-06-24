20c39ad5b2334970e6b981f1c0a283df6857afca
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-06-24 13:36:43 +0530

    commit

--- src/ArrayPractice/ArraySum.java
+++ src/ArrayPractice/ArraySum.java
@@ -1,11 +1,11 @@
 package ArrayPractice;
 
 public class ArraySum {
-    static int simpleArraysSum(int arr []) {
+    static int simpleArraysSum(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int j : arr) {
 //            sum = 0;
-            sum +=arr[i];
+            sum += j;
 //            return sum;
 
         }
@@ -14,7 +14,7 @@ public class ArraySum {
         return sum;
     }
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int ans = simpleArraysSum(arr);
         System.out.println(simpleArraysSum(arr));
 
