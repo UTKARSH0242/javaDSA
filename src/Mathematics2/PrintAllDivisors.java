8549c2c98823aab749ad46b3ed52ade28d7e4fed
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-06-28 13:26:48 +0530

    Java is Fun

--- src/Mathematics2/PrintAllDivisors.java
+++ src/Mathematics2/PrintAllDivisors.java
@@ -1,7 +1,25 @@
 package Mathematics2;
 
 public class PrintAllDivisors {
+    public static void printAllDivisor(int n){
+        int i = 1;
+        for (; i*i <=n ; i++) {
+            if (n % i == 0){
+                System.out.println(i);
+
+            }
+        }
+        i--;
+        for (; i >=1 ; i--) {
+            if (n%i == 0){
+                if (n/i != i) System.out.println(n/i);
+            }
+
+        }
+    }
     public static void main(String[] args) {
+        printAllDivisor(25);
+
 
     }
 }
